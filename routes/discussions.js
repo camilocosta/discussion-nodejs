f161f79f91e7d81157235fe6e2f93ef7c168326f
Camilo Costa <camilogilc@gmail.com>
2016-03-20 15:17:07 -0300

    Utilizar sendStatus

--- routes/discussions.js
+++ routes/discussions.js
@@ -8,7 +8,7 @@ exports.listDiscussions = function(req, res) {
 		{"title": 1, "tag": 1, "author": 1, "numberOfReplies": 1, "dateCreated": 1} )
 	.sort({dateCreated: -1})
 	.exec(function(error, discussions) {
-		if(error) res.send(500);
+		if(error) res.sendStatus(500);
 		res.json({ discussions: discussions });
 	});
 
@@ -21,7 +21,7 @@ exports.getDiscussion = function(req, res) {
 	db.Discussion.findOne({ _id: id })
 	.populate('replies._author', 'name username')
 	.exec(function(error, discussion) {
-		if(error) res.send(500);
+		if(error) res.sendStatus(500);
 		res.json({ discussion: discussion });
 	});
 
@@ -35,8 +35,8 @@ exports.saveDiscussion = function(req, res) {
 	discussion.author = discussion.author;
 	discussion.save(function(error, discussion) {
 		console.log(error);
-		if(error) res.send(500);
-		res.send(201);
+		if(error) res.sendStatus(500);
+		res.sendStatus(201);
 	});
 
 };
@@ -47,7 +47,7 @@ exports.saveReplyToDiscussion = function(req, res) {
 	var discussionID = req.params.id;
 	db.Discussion.findOne({ _id: discussionID }, function(error, discussion) {
 
-		if(error) res.send(500);
+		if(error) res.sendStatus(500);
 
 		var reply = req.body;
 
@@ -59,7 +59,7 @@ exports.saveReplyToDiscussion = function(req, res) {
 			if(error) {
 				console.log("Erro ao Salvar Resposta");
 				console.log(error);
-				res.send(500);
+				res.sendStatus(500);
 			}
 			
 			//TODO reply
